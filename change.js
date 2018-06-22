50b418e9e694e2b14109debffff54dbd6ee6c8d1
Anthony Hurtado <anhurtado@outlook.com>
2018-06-22 08:10:52 -0400

    Actualización del script para mejorar la visualización de los videos

--- change.js
+++ change.js
@@ -8,6 +8,10 @@ function changeVideo(code) {
 	let baseUrl = "https://www.youtube.com/embed/";
 	let currentVideo = baseUrl + code;
 
+	// Toggle Video (Ocultar y mostrar)
+	container.style.display = 'none';
+	setTimeout(function() { container.style.display = 'block'; }, 1000);
+
 	// Añadiendo el video actual
 	container.src = currentVideo;
 }
